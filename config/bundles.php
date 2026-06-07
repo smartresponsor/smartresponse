2b37631a298f304efa19384b8235a573f0891cbe
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-06 19:22:52 -0500

    Add cataloging to prod runtime

--- config/bundles.php
+++ config/bundles.php
@@ -32,4 +32,6 @@ return [
     App\Rolling\RollingBundle::class => ['all' => true],
     App\Applicating\ApplicatingBundle::class => ['all' => true],
     App\Searching\SearchingBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    App\Cataloging\CatalogingBundle::class => ['all' => true],
 ];
