b9f676f9610825f75030de8f84ebf36a548846fd
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-07 02:41:30 -0500

    Move guest storefront to showcase path

--- src/Service/Dashboard/AppDashboardBuilderService.php
+++ src/Service/Dashboard/AppDashboardBuilderService.php
@@ -131,7 +131,7 @@ final class AppDashboardBuilderService implements AppDashboardContract
     public function __invoke(Request $request): Response
     {
         return $this->navigationRenderer->render(
-            'guest',
+            'showcase',
             'index',
             $this->buildDashboard($request)->toArray(),
         );
