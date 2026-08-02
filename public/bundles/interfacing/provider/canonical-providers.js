026552905180ee4968d045a6c79d2d344d55a1c0
Oleksandr Tishchenko <dev@smartresponsor.com>
2026-08-02 01:41:01 -0500

    Publish updated Interfacing provider assets

--- public/bundles/interfacing/provider/canonical-providers.js
+++ public/bundles/interfacing/provider/canonical-providers.js
@@ -1,2 +1,2 @@
-import"./provider-registry.js";import"./providers/antd-pro.js";import"./providers/primereact.js";/* empty css                                            */import"./chunks/registry-V5OFkfe5.js";import"./chunks/navigation-me2RlgO4.js";import"./chunks/workbench-CCEbgzjT.js";
+import"./provider-registry.js";import"./providers/antd-pro.js";import"./providers/primereact.js";/* empty css                                            */import"./chunks/registry-V5OFkfe5.js";import"./chunks/navigation-0yh54Fjz.js";import"./chunks/workbench-CCEbgzjT.js";
 //# sourceMappingURL=canonical-providers.js.map
